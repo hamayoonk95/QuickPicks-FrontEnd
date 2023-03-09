f83f1d9c6cd296f8071148c2fc73d154c3ee37d1
Hamayoon <hamayoonk38@gmail.com>
2023-03-09 23:23:18 +0000

    api call response fixed, returned array instead of object

--- src/components/Random-movie/Random-movie.js
+++ src/components/Random-movie/Random-movie.js
@@ -29,7 +29,7 @@ const RandomMovie = () => {
     setCurrentIdx(currentIdx + 1); // increment the index of the current movie
     setMovie(movieData); // update the movie state with the fetched data
   };
-  
+
   // function to go to the previous movie
   const goToPrev = () => {
     if (currentIdx > 0 && prevMovies.length > 0) {
@@ -49,8 +49,7 @@ const RandomMovie = () => {
       vote_average,
       genres,
       id,
-    } = movie;
-
+    } = movie[0];
     // split the overview into words, reduce the number of words to 30
     const words = overview.split(" ");
     const reducedWords = words.slice(0, 30);
@@ -101,14 +100,14 @@ const RandomMovie = () => {
           <div className="info-container">
             <div className="title-rating">
               <h2 onClick={() => navigate(`/movie/${id}`)} className="title">
-                {title && title.split(" ").slice(0, 5).join(" ")} ({release_date})
+                {title.split(" ").slice(0, 5).join(" ")} ({release_date})
               </h2>
               {vote_average > 0 ? (
                 <div className="rating">{vote_average}</div>
               ) : null}
             </div>
             {/* render genres, split genre by '-' and join by ', ' */}
-            <div className="genre">{genres && genres.split("-").join(", ")}</div>
+            <div className="genre">{genres.split("-").join(", ")}</div>
             {/* render description, if word count > 30 display three dots ... */}
             <p className="description">
               {reducedDescription} {words.length > 30 && "..."}
