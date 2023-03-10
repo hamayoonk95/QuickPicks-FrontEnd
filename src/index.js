d68f875e828b576f428306c87a3fb5c1155e696a
Hamayoon <hamayoonk38@gmail.com>
2023-03-10 14:48:36 +0000

    added axios default with credentials = true

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import dotenv from 'dotenv';
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 dotenv.config();
 
