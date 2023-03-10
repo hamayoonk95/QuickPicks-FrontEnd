be2ef7274e664e576911ad2aba012808919f7c2c
Hamayoon <hamayoonk38@gmail.com>
2023-03-10 12:43:21 +0000

    added localStorage token as header of request

--- src/utils/useAxiosJWT.js
+++ src/utils/useAxiosJWT.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
@@ -10,7 +10,16 @@ export const useRefreshToken = () => {
 
   const refreshToken = async () => {
     try {
-      const response = await axios.get("https://www.doc.gold.ac.uk/usr/391/token");
+      const response = await axios.get(
+        "https://www.doc.gold.ac.uk/usr/391/token",
+        {
+          withCredentials: true,
+          credentials: "include",
+          headers: {
+            Authorization: `Bearer ${localStorage.accessToken}`,
+          },
+        }
+      );
       setToken(response.data.accessToken);
       const decoded = jwt_decode(response.data.accessToken);
       setExpire(decoded.exp);
@@ -23,14 +32,16 @@ export const useRefreshToken = () => {
   return [token, setToken, expire, setExpire, refreshToken];
 };
 
-export const useAxiosJWT = (token, setToken, setExpire ,expire) => {
+export const useAxiosJWT = (token, setToken, setExpire, expire) => {
   const axiosJWT = axios.create();
 
   axiosJWT.interceptors.request.use(
     async (config) => {
       const currentDate = new Date();
       if (expire * 1000 < currentDate.getTime()) {
-        const response = await axios.get("https://www.doc.gold.ac.uk/usr/391/token");
+        const response = await axios.get(
+          "https://www.doc.gold.ac.uk/usr/391/token"
+        );
         config.headers.Authorization = `Bearer ${response.data.accessToken}`;
         setToken(response.data.accessToken);
         const decoded = jwt_decode(response.data.accessToken);
